a910aa3540703ca85f5cef4f18be9430350f9a12
Rahtainka <rahtainka@gmail.com>
2018-05-01 16:32:27 +0200

    added error handling for invalid json

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.use(bodyParser.json());
 const words = require('./resources/words');
 const highscores = require('./resources/highscores');
 
+app.use(function(err, req, res, next) {
+    if (err.type === 'entity.parse.failed') {
+        return res.status(400).send(JSON.stringify({
+            error: {
+                code: "INVALID_JSON",
+                message: "The body of your request is not valid JSON."
+            }
+        }))
+    }
+});
+
 app.get('/highscore', (req, res) => {
     res.status(200).json(highscores);
 });
